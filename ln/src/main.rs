63298fdf3988e103f78fe2f5d400470a5375a6ff
IsaacCooke <cookeisaac2@gmail.com>
2024-03-13 22:06:59 +0000

    Separated unsafe commands into structs where possible

--- ln/src/main.rs
+++ ln/src/main.rs
@@ -1,6 +1,9 @@
 extern crate gl;
 extern crate sdl2;
 
+pub mod render_gl;
+
+use crate::render_gl::*;
 use std::ffi::{CStr, CString};
 
 fn main() {
@@ -22,6 +25,16 @@ fn main() {
         gl::ClearColor(0.3, 0.3, 0.5, 1.0);
     }
 
+    let vert_shader =
+        render_gl::Shader::from_vert_source(&CString::new(include_str!("vert.glsl")).unwrap())
+            .unwrap();
+    let frag_shader =
+        render_gl::Shader::from_frag_source(&CString::new(include_str!("frag.glsl")).unwrap())
+            .unwrap();
+
+    let shader_program = render_gl::Program::from_shaders(&[vert_shader, frag_shader]).unwrap();
+    shader_program.set_used();
+
     let mut event_pump = sdl.event_pump().unwrap();
     'main: loop {
         for event in event_pump.poll_iter() {
@@ -39,42 +52,4 @@ fn main() {
 
         window.gl_swap_window();
     }
-}
-
-fn shader_from_source(source: &CStr, kind: gl::types::GLenum) -> Result<gl::types::GLuint, String> {
-    let id = unsafe { gl::CreateShader(kind) };
-
-    unsafe {
-        gl::ShaderSource(id, 1, &source.as_ptr(), std::ptr::null());
-        gl::CompileShader(id);
-    }
-
-    let mut success: gl::types::GLint = 1;
-    unsafe {
-        gl::GetShaderiv(id, gl::COMPILE_STATUS, &mut success);
-    }
-    if success == 0 {
-        let mut len: gl::types::GLint = 0;
-        unsafe {
-            gl::GetShaderiv(id, gl::INFO_LOG_LENGTH, &mut len);
-
-            let error = create_whitespace_cstring_with_len(len as usize);
-
-            gl::GetShaderInfoLog(
-                id,
-                len,
-                std::ptr::null_mut(),
-                error.as_ptr() as *mut gl::types::GLchar,
-            );
-            return Err(error.to_string_lossy().into_owned());
-        }
-    }
-
-    Ok(id)
-}
-
-fn create_whitespace_cstring_with_len(len: usize) -> CString {
-    let mut buffer: Vec<u8> = Vec::with_capacity(len as usize + 1);
-    buffer.extend([b' '].iter().cycle().take(len as usize));
-    unsafe { CString::from_vec_unchecked(buffer) }
 }
