465d10ffc9177838cc7108d0af0f238fc8a86004
IsaacCooke <cookeisaac2@gmail.com>
2024-03-30 22:06:49 +0000

    Made progress?

--- sdl/main.cpp
+++ sdl/main.cpp
@@ -7,7 +7,7 @@
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
-enum KeyPressSurfaces {
+enum gKeyPressSurfaces {
   KEY_PRESS_SURFACE_DEFAULT,
   KEY_PRESS_SURFACE_UP,
   KEY_PRESS_SURFACE_DOWN,
@@ -74,15 +74,43 @@ bool init(){
 
 bool loadMedia()
 {
-  bool success = true;
+    bool success = true;
 
-  gHelloWorld = SDL_LoadBMP("hello.bmp");
-  if(gHelloWorld == NULL){
-    printf("Unable to load image %s! Error: %s\n", "hello.bmp", SDL_GetError());
-    success = false;
-  }
+    gKeyPressSurfaces[KEY_PRESS_SURFACE_DEFAULT] = loadSurface("press.bmp");
+    if(gKeyPressSurfaces[KEY_PRESS_SURFACE_DEFAULT] == NULL) {
+        printf("Failed to load the default image\n");
+        success = false;
+    }
 
-  return success;
+    // Up surface
+    gKeyPressSurfaces[KEY_PRESS_SURFACE_UP] = loadSurface("up.bmp");
+    if(gKeyPressSurfaces[KEY_PRESS_SURFACE_UP] == NULL) {
+        printf("Failed to load up image\n");
+        success = false;
+    }
+
+    // Down surface
+    gKeyPressSurfaces[KEY_PRESS_SURFACE_DOWN] = loadSurface("down.bmp");
+    if(gKeyPressSurfaces[KEY_PRESS_SURFACE_DOWN] == NULL) {
+        printf("Failed to load down image!\n");
+        success = false;
+    }
+
+    // Left surface
+    gKeyPressSurfaces[KEY_PRESS_SURFACE_LEFT] = loadSurface("left.bmp");
+    if(gKeyPressSurfaces[KEY_PRESS_SURFACE_LEFT] == NULL) {
+        printf("Failed to load left image!\n");
+        success = false;
+    }
+
+    // Right surface
+    gKeyPressSurfaces[KEY_PRESS_SURFACE_RIGHT] = loadSurface("right.bmp");
+    if(gKeyPressSurfaces[KEY_PRESS_SURFACE_RIGHT] == NULL) {
+        printf("Failed to load right image!\n");
+        success = false;
+    }
+
+    return success;
 }
 
 SDL_Surface* loadSurface(std::string path){
